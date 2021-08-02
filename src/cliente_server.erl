d5076b25171c778d04337d0136c81932ae5cc99e
ricardpons <ricardponsramos@gmail.com>
2021-08-02 11:16:16 +0200

    Add files via upload
    Add PropEr test

--- src/cliente_server.erl
+++ src/cliente_server.erl
@@ -1,10 +1,11 @@
 -module(cliente_server).
 -behaviour(gen_server).
 -export([start_link/0, temp/1, get_cont/0]).
--export([init/1, handle_call/3, handle_cast/2, terminate/2]).
+-export([init/1, handle_call/3, handle_cast/2, terminate/2,mayor/1]).
+-include_lib("proper/include/proper.hrl").
 -include_lib("eunit/include/eunit.hrl").
 -define(concuerror_options, [{module, ?MODULE}, quiet]).
--record(estado, {cont::float()}).
+-record(estado, {cont}).
 
 
 %% Client API
@@ -21,18 +22,20 @@ get_cont() ->
 
 %% Server functions
 init({}) ->
-    { #estado{cont=0.0}}.
+    { ok,#estado{cont=0.0}}.
 
 handle_cast({temp, Num}, #estado{cont = Cont} = Estado) ->
     Cels = Num*  9 / 5 + 32,
     Ger=float_to_list(Cels,[{decimals,2}]),
     io:format("La temperatura en grados Fahrenheit es ~s!~n",[Ger]),
-    {reply, Estado#estado{cont =Ger  }};
+    {noreply, Estado#estado{cont =Ger }};
 
 handle_cast(Msg, Estado) ->
     error_logger:warning_msg("Bad message: ~p~n", [Msg]),
     {noreply, Estado}.
 
+handle_call({get_cont},_From,Estado) ->
+    {reply,{ok,Estado#estado.cont},Estado};
 
 handle_call(Request, _From, Estado) ->
     error_logger:warning_msg("Bad message: ~p~n", [Request]),
@@ -47,3 +50,13 @@ terminate(_Reason, _Estado) ->
      ?assertEqual({noreply,9.0},handle_cast({temp,36.5},90.70)).
   new_test() ->
     ?assertEqual({noreply,90.70},handle_cast({temp,36.5},90.70)).
+  new1_test() ->
+    ?assertEqual({noreply,k},handle_cast({temp,36.5},90.70)).
+
+prop_cliente() ->
+    ?FORALL(T, float(),mayor(T)).
+
+mayor(T) ->
+    T >=-273.15.
+
+
